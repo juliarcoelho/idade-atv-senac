96b79fb034d12452f4acd01d1df976bb80876d4f
juliarcoelho <juliarochx@gmail.com>
2023-05-31 16:06:40 -0300

    adicionando arquivos no git

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int dia, mes, ano, idade;
+
+            Console.Clear();
+            Console.WriteLine("Digite o seu nome:");
+            nome = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Olá " + nome);
+
+            Console.WriteLine("Digite o seu dia de nascimento:");
+            dia = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Digite o seu mês de nascimento:");
+            mes = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Digite o seu ano de nascimento:");
+            ano = Convert.ToInt16(Console.ReadLine());
+            
+            Console.Clear();
+
+            idade = 2021 - ano;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Senha sugerida: " + idade + nome);
+            }
+            else
+            {
+                Console.WriteLine("Senha sugerida: " + nome + idade);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Gerador de Senha com base na Idade do Usuário
+
+Este é um programa em C#, feito como atividade do curso de TI do Senac, que solicita os dados do usuário e sugere uma senha com base na idade fornecida. O programa verifica se o usuário é menor de idade ou maior de idade e gera uma senha correspondente.
+
+## Funcionalidades
+
+- Solicita ao usuário que insira seus dados pessoais, incluindo nome e data de nascimento.
+- Verifica se o usuário é menor de idade ou maior de idade.
+- Sugere uma senha com base nesses dados.
+
+## Pré-requisitos
+
+- Ambiente de desenvolvimento compatível com C# 
+- .NET Framework (ou versão superior)
